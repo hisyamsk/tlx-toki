840be90808e250643e09b0a31acf51d09d8c544c
hisyamsk <56929668+hisyamsk@users.noreply.github.com>
2020-07-17 20:18:24 +0700

    Add files via upload

--- pemrogaman-dasar/12 Rekursi/12_e.cpp
+++ pemrogaman-dasar/12 Rekursi/12_e.cpp
@@ -4,20 +4,19 @@
 
 using namespace std;
 
-int f(int x) {
-	if (x == 1) {
-		return 1;
-	}
-	if (x % 2 == 0) {
-		return (x/2) * f(x-1);
+string f(int n) {
+	if (n == 1) {
+		return "1";
+	} else if (n % 2 == 1) {
+		return f(n/2) + "1";
 	} else {
-		return x * f(x-1);
-	}
+		return f(n/2) + "0";
+	}	
 }
 
 int main() {
 	int n;
 	cin >> n;
-	int res = f(n);
+	string res = f(n);
 	cout << res << endl;
 }
