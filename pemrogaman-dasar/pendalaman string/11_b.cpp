7fc2971b0b61e8bee230d997caeb643bf7235fff
hisyamsk <56929668+hisyamsk@users.noreply.github.com>
2020-07-17 19:56:29 +0700

    Create 11_b.cpp

---
+++ pemrogaman-dasar/pendalaman string/11_b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+	string s, t;
+	cin >> s >> t;
+	while (s.find(t) != -1) {
+		s.erase(s.find(t), t.length());
+	}
+	cout << s << endl;
+}
+			
