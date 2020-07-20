dddee05c5b3d17365ee94115ab7b622a882fa742
Hisyam Setiadi Kurniawan <hisyamskurniawan18@gmail.com>
2020-07-20 10:17:10 +0700

    Create 5_b.cpp

---
+++ pemrogaman-dasar/05 Percabangan/5_b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+// modul cpp-3 halaman 21
+using namespace std;
+
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	if (n > 0)
+	{
+		printf("%d\n", n);
+	
