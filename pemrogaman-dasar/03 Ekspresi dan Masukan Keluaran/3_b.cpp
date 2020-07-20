318f33cc3109e0bd9b5685375abd6a8eec56ecaa
Hisyam Setiadi Kurniawan <hisyamskurniawan18@gmail.com>
2020-07-20 10:23:42 +0700

    Create 3_b.cpp

---
+++ pemrogaman-dasar/03 Ekspresi dan Masukan Keluaran/3_b.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+
+using namespace std;
+
+int a, b, hasil;
+
+int main() 
+{
+    scanf("%d %d", &a, &b);
+    hasil = a + b;
+    printf("%d\n", hasil);
+}
